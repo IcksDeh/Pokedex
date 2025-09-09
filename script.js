5c7df494ffd88cd6b6208fdc3f52fe65bedfa689
Your NameIcksDeh <diagnose-neongruen@gmx.de>
2025-09-09 18:49:59 +0200

    show pictue of each pokemon. fetched each pokemon url for detailed information

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 let allPokemon = [];
+let detailPokemonInfo = []; 
 
 function init(){
     loadPokemon()
 }
 
 async function loadPokemon (){
-    let fetchPokemonAPI = await fetch("https://pokeapi.co/api/v2/pokemon?limit=5&offset=0");
-        console.log(fetchPokemonAPI);
-    let pokemon = await fetchPokemonAPI.json();
-        console.log(pokemon);
-    allPokemon = pokemon.results;
+    const fetchPokemonAPI = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10&offset=0");
+    const pokemon = await fetchPokemonAPI.json();
 
-        console.log(allPokemon);
-   
-    showPokemon();
+    allPokemon = pokemon.results;
+    fetchSinglePokemon(); 
+}
+
+async function fetchSinglePokemon(){
     
+    for (let pokeIndex = 0; pokeIndex < allPokemon.length; pokeIndex++) {
+        
+        let fetchSinglePokemonByApi = await fetch (allPokemon[pokeIndex].url);
+        let singlePokemon = await fetchSinglePokemonByApi.json();
+        detailPokemonInfo.push(singlePokemon); 
+    }
+    console.log(detailPokemonInfo);
+    showPokemon();
 }
 
 function showPokemon(){
     let showPokemonInHTML = document.getElementById("id_show_pokemon");
-    for (let pokeIndex = 0; pokeIndex < allPokemon.length; pokeIndex++) {
+    for (let pokeIndex = 0; pokeIndex < detailPokemonInfo.length; pokeIndex++) {
         showPokemonInHTML.innerHTML += templatePokemonCard(pokeIndex);
-        
-        console.log(allPokemon[pokeIndex].url);
 
-        fetchSinglePokemon(allPokemon[pokeIndex].url);
     }
 }
 
-async function fetchSinglePokemon(url){
-    let fetchSinglePokemonByApi = await fetch (url);
-    let singlePokemon = await fetchSinglePokemonByApi.json();
-    console.log(singlePokemon); 
-}
--- scripts/templates.js
+++ scripts/templates.js
@@ -2,10 +2,10 @@ function templatePokemonCard(pokeIndex){
     return `<div class="pokemon_card_background_color">
                     <div class="sort_id_name_pokemon">
                         <p>${pokeIndex+1}</p>
-                        <p>${allPokemon[pokeIndex].name}</p>
+                        <p>${detailPokemonInfo[pokeIndex].name}</p>
                     </div>    
                     <div class="sort_img_pokemon">
-                        <img class="img_pokemon" src="" alt="img Pokemon">
+                        <img class="img_pokemon" src="${detailPokemonInfo[pokeIndex].sprites.front_default}" alt="img Pokemon">
                     </div>
                 </div>`
 }
